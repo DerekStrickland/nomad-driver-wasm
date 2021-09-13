a2142e2befeaafa93689004e2914e0f2c95e9c45
DerekStrickland <dstrickland@hashicorp.com>
2021-09-13 11:08:05 -0400

    Patch with fixed prost and add some basic task driver config

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -1,6 +1,13 @@
+#[path = "hashicorp.nomad.plugins.base.proto.rs"]
+pub mod base;
+
 #[path = "hashicorp.nomad.plugins.drivers.proto.rs"]
 pub mod drivers;
+
+pub mod hclext;
+
 #[path = "hashicorp.nomad.plugins.shared.hclspec.rs"]
 pub mod hclspec;
+
 #[path = "hashicorp.nomad.plugins.shared.structs.rs"]
 pub mod structs;
