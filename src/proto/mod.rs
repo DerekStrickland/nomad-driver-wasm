0a393459b55de25525d8133635536824faa7b800
DerekStrickland <dstrickland@hashicorp.com>
2021-09-21 06:22:49 -0400

    Protobuf generating but paths is wrong in generated drivers file

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -9,10 +9,8 @@ pub mod hclext;
 #[path = "hashicorp.nomad.plugins.shared.hclspec.rs"]
 pub mod hclspec;
 
+#[path = "google.protobuf.rs"]
+pub mod protobuf;
+
 #[path = "hashicorp.nomad.plugins.shared.structs.rs"]
 pub mod structs;
-
-mod driversext;
-
-#[path = "google.protobuf.rs"]
-mod google_protobuf;
